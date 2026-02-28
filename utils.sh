974b981dd6226d7493c209cd016ac85de9035738
Wolfcity <132693162+sixstrings@users.noreply.github.com>
2026-02-28 15:56:46 +0100

    Changein module.prop generation
    Signed-off-by: Wolfcity <132693162+sixstrings@users.noreply.github.com>

--- utils.sh
+++ utils.sh
@@ -701,7 +701,7 @@ module_prop() {
 name=${2}
 version=${3}
 versionCode=${NEXT_VER_CODE}
-author=peternmuller
+author=sixstrings 
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MODULE_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
